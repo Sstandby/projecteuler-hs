21789c33da7f46d61d8b93de3cb80f1b277592e7
Miracle <datm2727@gmail.com>
2022-08-06 23:30:52 -0500

    lista de primos usando la criba de Eratóstenes

--- Problem-003/solution-m.hs
+++ Problem-003/solution-m.hs
@@ -1,26 +1,13 @@
 -- Encontraremos al factor primo mayor dividiendo recursivamente al numero dado entre
 -- sus factore primos pequeños hasta que no se pueda dividir más
 
--- Raiz cuadrada redondeada, función para hacer más limpio el código
-sqrtRounded :: Integer -> Integer
-sqrtRounded n = round (sqrt (fromIntegral n))
-
--- Creamos la función prime que nos dirá si un número es primo o no
--- (Intente hacer que la función sea lo más rápida posible)
-isPrime :: Integer -> Bool
-isPrime 1 = False
-isPrime n = go (sqrtRounded n) 2
-    where
-        go :: Integer -> Integer -> Bool
-        go m d
-            | d > sqrtRounded n = True
-            | mod n d == 0 = False
-            | odd d = go m (d + 2)
-            | otherwise = go m (d + 1)
-
 -- La lista de primos que usaremos
-listOfPrimes :: [Integer]
-listOfPrimes = [x | x <- 2 : [3, 5 ..], isPrime x]
+sieve :: [Integer] -> [Integer]
+sieve [] = []
+sieve (p:xs) = p : sieve [x | x <- xs, mod x p > 0]
+
+primes :: [Integer]
+primes = sieve [2 ..]
 
 largestPrimeFactor :: Integer -> [Integer] -> Integer
 largestPrimeFactor n [] = 0 -- Solo sirve para completar patrones, al final de cuentas la lista de primos es infinita
@@ -31,4 +18,4 @@ largestPrimeFactor n (x:xs)
 
 -- Respuesta
 result :: Integer
-result = largestPrimeFactor 600851475143 listOfPrimes
+result = largestPrimeFactor 600851475143 primes
--- Problem-007/solution-m.hs
+++ Problem-007/solution-m.hs
@@ -1,19 +1,11 @@
--- Función que nos dice si un número es primo
-isPrime :: Integer -> Bool
-isPrime 1 = False
-isPrime n = go ((round.sqrt.fromIntegral) n) 2
-    where
-        go :: Integer -> Integer -> Bool
-        go m d
-            | d > (round.sqrt.fromIntegral) n = True
-            | mod n d == 0 = False
-            | odd d = go m (d + 2)
-            | otherwise = go m (d + 1)
-
 -- La lista de los primeros números primos
-listOfPrimes :: [Integer]
-listOfPrimes = [x | x <- 2 : [3, 5 ..], isPrime x]
+sieve :: [Integer] -> [Integer]
+sieve [] = []
+sieve (p:xs) = p : sieve [x | x <- xs, mod x p > 0]
+
+primes :: [Integer]
+primes = sieve [2 ..]
 
 -- Respuesta
 result :: Integer
-result = last (take 10001 listOfPrimes)
+result = last (take 10001 primes)
